de3c13b73be3ed3d24d7ff258ee0abe719b3fec9
bitorhugo <vitorhugosantos144@gmail.com>
2022-11-21 22:43:20 +0000

    changed how showcase was written on screen in homepage

--- resources/views/partials/showcase.blade.php
+++ resources/views/partials/showcase.blade.php
@@ -1,6 +1,6 @@
 <div class="container-fluid pt-5">
         <div class="text-center mb-4">
-            <h2 class="section-title px-5"><span class="px-2">Showcase</span></h2>
+            <h2>Showcase</h2>
         </div>
         <div class="row px-xl-5 pb-3">
             <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
