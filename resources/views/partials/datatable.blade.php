7e3e7fcc58bfd9f1fe54f0742e246a8ade13aea1
bitorhugo <vitorhugosantos144@gmail.com>
2022-11-22 00:26:31 +0000

    edited datatable

--- resources/views/partials/datatable.blade.php
+++ resources/views/partials/datatable.blade.php
@@ -6,19 +6,46 @@
                 <div class="table-data__tool">
                     <div class="table-data__tool-left">
                         <div class="rs-select2--light rs-select2--md">
-                            <select class="js-select2" name="property">
+                            <select class="js-select2 select2-hidden-accessible" name="property" tabindex="-1" aria-hidden="true">
                                 <option selected="selected">All Properties</option>
                                 <option value="">Option 1</option>
                                 <option value="">Option 2</option>
                             </select>
+                            <span class="select2 select2-container select2-container--default select2-container--focus" dir="ltr" style="width: 125px;">
+                                <span class="selection">
+                                    <span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false"
+                                          tabindex="0" aria-labelledby="select2-property-o3-container">
+                                        <span
+                                            class="select2-selection__rendered" id="select2-property-o3-container" title="All Properties">All Properties
+                                        </span>
+                                        <span class="select2-selection__arrow" role="presentation"><b role="presentation"></b>
+                                        </span>
+                                    </span>
+                                </span>
+                                <span class="dropdown-wrapper" aria-hidden="true">
+                                </span>
+                            </span>
                             <div class="dropDownSelect2"></div>
                         </div>
                         <div class="rs-select2--light rs-select2--sm">
-                            <select class="js-select2" name="time">
+                            <select class="js-select2 select2-hidden-accessible" name="time" tabindex="-1" aria-hidden="true">
                                 <option selected="selected">Today</option>
                                 <option value="">3 Days</option>
                                 <option value="">1 Week</option>
                             </select>
+                            <span class="select2 select2-container select2-container--default" dir="ltr" style="width: 75px;">
+                                <span class="selection">
+                                    <span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false"
+                                          tabindex="0" aria-labelledby="select2-time-kr-container">
+                                        <span class="select2-selection__rendered" id="select2-time-kr-container" title="Today">Today
+                                        </span>
+                                        <span class="select2-selection__arrow" role="presentation"><b role="presentation"></b>
+                                        </span>
+                                    </span>
+                                </span>
+                                <span class="dropdown-wrapper" aria-hidden="true">
+                                </span>
+                            </span>
                             <div class="dropDownSelect2"></div>
                         </div>
                         <button class="au-btn-filter">
