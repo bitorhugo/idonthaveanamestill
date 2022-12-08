9a13bb283c2b6b30d70e44aeb234c83b2c397a19
bitorhugo <vitorhugosantos144@gmail.com>
2022-12-08 06:00:45 +0000

    maaek resource controller for inventory

--- app/Http/Controllers/Payment/StripeController.php
+++ app/Http/Controllers/Payment/StripeController.php
@@ -15,7 +15,7 @@ class StripeController extends Controller
     public function checkout()
     {
         Stripe\Stripe::setApiKey(config('stripe.sk'));
-        
+
         $cart = \Cart::session(Auth::id())->getContent();
 
         $line_items = array();
@@ -36,16 +36,16 @@ class StripeController extends Controller
                 ];
             array_push($line_items, $line_data);
         }
-        
+
         $session = Stripe\Checkout\Session::create([
             'line_items'  => $line_items,
             'mode'        => 'payment',
-            'success_url' => route('paymentSuccess'),
+            'success_url' => route('paymentSuccess', ['items_bought' => $cart]),
             'cancel_url'  => route('paymentCanceled'),
         ]);
         return redirect()->away($session->url);
     }
-    
+
     public function payNow(Request $request)
     {
         Stripe\Stripe::setApiKey(config('stripe.sk'));
@@ -64,42 +64,51 @@ class StripeController extends Controller
                 ],
             ],
             'mode'        => 'payment',
-            'success_url' => route('paymentSuccess'),
+            'success_url' => route('paymentSuccess', ['item_id' => $request->id]),
             'cancel_url'  => route('paymentCanceled', ['search' => $request->id]),
         ]);
 
         return redirect()->away($session->url);
     }
 
-    public function success()
+    /**
+     * @TODO make inventory resource controller
+     */
+    public function success(Request $request)
     {
+        // handle respose from payNow 
+        if($request->has('item_id')) {
+            
+        }
         return redirect()->route('home')->with('success', 'Payment was fullfilled.');
     }
 
     public function canceled(Request $request)
     {
-        if($request->has('search')) {
+                ddd($request);
+        if ($request->has('search')) {
             return redirect()->route('search.show', ['search' => $request->search])
-                             ->with('error', 'Payment Canceled.');
+                ->with('error', 'Payment Canceled.');
         }
         return redirect()->route('cart.index')
-                         ->with('error', 'Payment Canceled.');
+            ->with('error', 'Payment Canceled.');
     }
 
     private function isCartCheckout()
     {
         $cart = \Cart::session(Auth::id())->getContents();
         $line_items = array();
-        foreach($cart as $item) {
+        foreach ($cart as $item) {
             $line_items = array_push(
-                ['price_data' => [
-                    'currency' => 'eur',
-                    'product_data' => [
-                       'name' => $item->name,
+                [
+                    'price_data' => [
+                        'currency' => 'eur',
+                        'product_data' => [
+                            'name' => $item->name,
+                        ],
+                        'unit_amount' => $item->price * 100,
                     ],
-                    'unit_amount' => $item->price * 100,
-                ],
-                 'quantity' => $item->quantity,
+                    'quantity' => $item->quantity,
                 ]
             );
         }
@@ -113,6 +122,4 @@ class StripeController extends Controller
 
         return $session;
     }
-
-    
 }
