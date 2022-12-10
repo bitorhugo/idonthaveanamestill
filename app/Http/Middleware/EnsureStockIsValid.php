e36a40132e1686466cf5285036ee83d4d735dd9f
bitorhugo <vitorhugosantos144@gmail.com>
2022-12-10 20:27:07 +0000

    addde EnsureStockisvalid middleware

---
+++ app/Http/Middleware/EnsureStockIsValid.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class EnsureStockIsValid
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        return $request->stock > 0
+            ? $next($request)
+            : redirect()->route('search.show', ['search' => $request->id])
+                        ->with('error', 'Out of Stock.');
+    }
+}
