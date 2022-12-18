c47185170b07b43fab99af24d3d1f7f4a3442b3a
bitorhugo <vitorhugosantos144@gmail.com>
2022-12-18 02:57:09 +0000

    configured image upload path

--- resources/views/partials/recentCards.blade.php
+++ resources/views/partials/recentCards.blade.php
@@ -7,10 +7,10 @@
             <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
                 <div class="card product-item border-0 mb-4">
                     <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                        @if(!$card->getMedia()->isEmpty())
-                            <img class="" src="{{asset('storage/media/'.$card->id.'/0.jpg')}}" alt='image'>
+                        @if(Storage::disk('media')->exists($card->id))
+                            <img src="{{asset('storage/media/'.$card->id.'/0.jpg')}}" alt='image'>
                         @else
-                            <img class="" src="{{asset('storage/baseImage.jpg')}}" alt='image'>
+                            <img src="{{asset('storage/baseImage.jpg')}}" alt='image'>
                         @endif
                     </div>
                      <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
