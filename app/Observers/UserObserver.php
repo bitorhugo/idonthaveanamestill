ecb6cf9eb81b07975ec55190fcbd3783cbacbbb6
bitorhugo <vitorhugosantos144@gmail.com>
2023-01-12 00:09:20 +0000

    added user observer

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Cache;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        $this->clearCache();
+    }
+
+    /**
+     * Handle the User "updated" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        $this->clearCache();
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        $this->clearCache();
+    }
+
+    /**
+     * Handle the User "restored" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        $this->clearCache();
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        $this->clearCache();
+    }
+
+
+    private function clearCache(){
+        collect(range(1, 100))->each(function ($i) {
+            $key = 'users-page-' . $i;
+            if(Cache::has($key)) {
+                Cache::forget($key);
+            }
+            else return false;
+        });
+    }
+}
